471387f87f4accac6f543a491a43645d91729ef0
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-01-23 14:04:26 -0300

    update_v5

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let faq = document.getElementsByClassName("question_button");
+let buttonFaq = document.getElementsByClassName("question_button");
 let i;
-for (i = 0; i < faq.length; i++) {
-  faq[i].addEventListener("click", function () {
+for (i = 0; i < buttonFaq.length; i++) {
+  buttonFaq[i].addEventListener("click", function () {
     this.classList.toggle("active");
     this.style.fontWeight = "700";
     let body = this.nextElementSibling;
