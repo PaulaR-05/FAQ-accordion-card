682508f2f7750835a256c4dd9df0a4c0e9c6f88a
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-01-27 08:44:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let buttonFaq = document.getElementsByClassName("question_button");
+let img = document.querySelectorAll(".img-icon");
 let i;
 for (i = 0; i < buttonFaq.length; i++) {
   buttonFaq[i].addEventListener("click", function () {
@@ -7,6 +8,7 @@ for (i = 0; i < buttonFaq.length; i++) {
     let body = this.nextElementSibling;
     if (body.style.display === "block") {
       body.style.display = "none";
+      this.style.fontWeight = "";
     } else {
       body.style.display = "block";
     }
